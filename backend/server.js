0d5a7754230562aa320083cfd67d34dedc01d112
Muhammmad Zain <zainchoudery786@gmail.com>
2026-01-20 17:53:18 +0500

    a

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,9 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import path from 'path';
 
 dotenv.config();
 
-const __dirname = path.resolve();
 
 const app = express();
 const port = process.env.PORT;
@@ -21,24 +19,6 @@ app.get('/api/message', (req, res) => {
     });
 });
 
-// For Production
-if (process.env.NODE_ENV === "production") {
-    // //app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-    // app.get("/{*any}", (req, res) => {
-    //     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-    // })
-
-    // __dirname is the 'backend' folder. 
-    // Since 'mv' puts 'dist' inside 'backend', the path is now:
-    const distPath = path.join(__dirname, 'dist');
-
-    app.use(express.static(distPath));
-
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(distPath, 'index.html'));
-    });
-}
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
