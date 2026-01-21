aacdc71c3c3406bb7a2bbf106f33fdcec1744cfa
Muhammmad Zain <zainchoudery786@gmail.com>
2026-01-21 15:01:22 +0500

    cors added

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT||3000;
+
+app.use(cors());
  
 app.get('/', (req, res) => {
   res.json({ message: 'Hello from Express on Vercel!' });
