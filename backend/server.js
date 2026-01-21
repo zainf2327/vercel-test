cff82bd94a2848694a3ff2ab04c3c9d923708f62
Muhammmad Zain <zainchoudery786@gmail.com>
2026-01-21 14:32:40 +0500

    backend ready for vercel

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,15 @@
 import express from 'express';
-import cors from 'cors';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-
 const app = express();
-const port = process.env.PORT;
-
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
-app.use(express.json());
-
-
-app.get('/api/message', (req, res) => {
-    res.json({
-        message: 'Hello from Express Backend!',
-        timestamp: new Date().toISOString()
-    });
+const port = process.env.PORT||3000;
+ 
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello from Express on Vercel!' });
 });
-
-
+ 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Example app listening on port ${port}`);
 });
